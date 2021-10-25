35454c63e7f80e9eb0d22a296f12b69be548f8a9
shrvya <shrivyashetty@gmail.com>
2021-10-25 15:14:50 +0530

    added:database

--- config/database.user.js
+++ config/database.user.js
@@ -1,6 +1,5 @@
 const dbConfig = require('./database.config');
 const mongoose = require('mongoose');
-//const { MONGO_URI } = process.env;
 const dbConnect = () => {
     mongoose.connect(dbConfig.url, {useNewUrlParser: true}).then(() => {
         console.log("Successfully connected to the database");
